69591887f0e28bf0f27d874a9e7950487be8bd45
Gustavo <gdssouza@id.uff.br>
2021-03-30 19:23:11 -0300

    refactor: remove test

--- selectSort.py
+++ selectSort.py
@@ -22,21 +22,9 @@ def selectSort(array):
         list: ordered copy array
     """
     array = array.copy()
-    for i in range(len(array)):
+    n = len(array)
+    for i in range(n):
         minValue, minIndex = minimum(array[i:])
         array[i+minIndex] = array[i]
         array[i] = minValue
     return array
-
-# array original
-array = [5, 4, 6, 2, 1, 7, 0, 3, 8, 9]
-# resultado
-array_select = selectSort(array)
-# gabarito
-gabarito = array.copy()
-gabarito.sort()
-
-# saida
-print("Array Original: ", array)
-print("Array Select:   ", array_select)
-print("Array Gabarito: ", gabarito)
